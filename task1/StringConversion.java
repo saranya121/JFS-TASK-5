9d5b2e15fa3dff5d0565928a5e9471e2f742baea
saranya g <saranyagajendran30@gmail.com>
2024-02-29 00:13:39 +0530

    Update StringConversion.java

--- task1/StringConversion.java
+++ task1/StringConversion.java
@@ -1,35 +1,27 @@
 package advanceJava.task1;
 
-import java.util.Scanner;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StringConversion {
-	public static void main(String a[]) {
-		/*
-		 * Get the no. of Strings to be added
-		 */
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the no of strings to be added in the list:");
-		int n = sc.nextInt();
-		String[] inputString = new String[n];
-		/*
-		 * Get the input from the user
-		 */
-		for (int i = 0; i < n; i++) {
-			System.out.println("Enter the String" + (i + 1) + ":");
-			inputString[i] = sc.next();
-		}
-		/*
-		 * Setting the array of string to Stream class
-		 */
-		Stream<String> st = Stream.of(inputString);
-		System.out.println("After changing the string in uppercase:");
-		/*
-		 * Converting the list of string in an array to uppercase
-		 */
-		System.out.println(st.map(s -> s.toString().toUpperCase()).collect(Collectors.toList()));
+	/**
+	 * Display List values
+	 */
+	public void displayList(List<String> list) {
+		list.forEach(System.out::println);
 
+	}
+
+	/**
+	 * Method to convert list of string to upper case
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public void convertStringToUpperCase(List<String> list) {
+		System.out.println("Non-Empty Strings in the List:");
+		list = list.stream().map(s -> s.toString().toUpperCase()).collect(Collectors.toList());
+		displayList(list);
 	}
 
 }
