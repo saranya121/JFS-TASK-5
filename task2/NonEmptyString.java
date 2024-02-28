91f2bf93262ee7f2212092e64d4ac44dc40798ab
saranya g <saranyagajendran30@gmail.com>
2024-02-29 00:16:59 +0530

    Update NonEmptyString.java

--- task2/NonEmptyString.java
+++ task2/NonEmptyString.java
@@ -1,41 +1,27 @@
 package advanceJava.task2;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class NonEmptyString {
+	/**
+	 * Display List values
+	 */
+	public void displayList(List<String> list) {
+		list.forEach(System.out::println);
 
-	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		Scanner sc = new Scanner(System.in);
-		/*
-		 * Get the no. of Strings to be added
-		 */
-		System.out.println("Enter the no of strings to be added in the list:");
-		int n = sc.nextInt();
-		/*
-		 * Get the input from the user
-		 */
-		System.out.println("Enter the String:");
-		for (int i = 0; i <= n; i++) {
-			String inputString = sc.nextLine();
-			list.add(inputString);
-		}
-		/*
-		 * Displaying the added List
-		 */
-		System.out.println("Added String in the List:" + list);
-		/*
-		 * Checking whether list of string is empty or not and setting into another list
-		 */
-		List nonEmptyList = list.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
-		/*
-		 * Displaying the non-empty String in the List
-		 */
+	}
+
+	/**
+	 * Method to convert list of string to upper case
+	 * 
+	 * @param list
+	 * @return
+	 */
+	public void getNonEmptyList(List<String> list) {
 		System.out.println("Non-Empty Strings in the List:");
-		nonEmptyList.forEach(System.out::println);
+		list = list.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+		displayList(list);
 	}
 
 }
